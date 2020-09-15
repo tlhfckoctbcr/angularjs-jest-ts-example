9aad88e847c14ff589deb9af3755ed7aa14180da
grantsru <grantsru@gmail.com>
2020-09-15 09:53:19 -0400

    Reorg, each component gets own file w/ test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import angular from 'angular';
-import Container from './components/container.component';
-import Counter from './components/counter.component';
+import Container from './components/container/container.component';
+import Counter from './components/counter/counter.component';
 
 export const appModule = angular.module('CounterApp', [])
   .component('container', Container)
--- src/components/container/container.component.spec.ts
+++ src/components/container/container.component.spec.ts
@@ -1,6 +1,3 @@
-import 'angular';
-import 'angular-mocks';
-
 describe('Container', () => {
   it('should equal 1', () => {
     expect(1).toEqual(1);
