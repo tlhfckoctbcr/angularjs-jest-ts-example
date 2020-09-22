250f7dac88518a281d700b180415164b8ea4f445
grantsru <grantsru@gmail.com>
2020-09-22 13:42:56 -0400

    Test ngSanitize HTML injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import angular from 'angular';
+import 'angular-sanitize';
+
 import Container from './components/container/container.component';
 import Counter from './components/counter/counter.component';
 
-export const appModule = angular.module('CounterApp', [])
+export const appModule = angular.module('CounterApp', ['ngSanitize'])
   .component('container', Container)
   .component('counter', Counter);
--- src/components/counter/counter.component.ts
+++ src/components/counter/counter.component.ts
@@ -2,6 +2,8 @@ export default {
   controller: class Counter {
     private count = 0;
 
+    private yesButton = '<div color="red">Some very bad things have happened. I don\'t know what, but it seems like this is the end for us.</div>';
+
     $onInit() {
       this.count = 1;
     }
@@ -20,6 +22,7 @@ export default {
           <input ng-model="$ctrl.count"></div>
           <button ng-click="$ctrl.increment()">+</button>
           <button ng-click="$ctrl.decrement()">-</button>
+          <div ng-bind-html="$ctrl.yesButton"></div>
       </div>
     `;
   }
